89be44a8eeeb2cceae3e1f684b8ea9e3a7e5b5bc
cakilgan <cakilganhan@gmail.com>
2026-01-17 22:27:47 +0300

    Initial commit

---
+++ src/main/java/com/github/ozgurkardesler/keepitsecret/KeepItSecret.java
@@ -0,0 +1,9 @@
+package com.github.ozgurkardesler.keepitsecret;
+
+import net.neoforged.fml.common.Mod;
+import static com.github.ozgurkardesler.keepitsecret.other.Reference.*;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(MOD_ID)
+public class KeepItSecret {
+}
---
+++ src/main/java/com/github/ozgurkardesler/keepitsecret/other/Reference.java
@@ -0,0 +1,10 @@
+package com.github.ozgurkardesler.keepitsecret.other;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Reference {
+    public static final String MOD_ID = "keepitsecret";
+    public static final String MOD_NAME = "KeepItSecret";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+}
